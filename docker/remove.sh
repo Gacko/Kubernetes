648617b2de7bf293860cc176dda55ba1d2744c9a
Marco Ebert <marco_ebert@icloud.com>
2017-07-30 22:29:42 +0200

    Docker: Remove everything.

--- docker/remove.sh
+++ docker/remove.sh
@@ -5,6 +5,7 @@ systemctl disable docker
 
 # Remove package.
 yum autoremove --assumeyes docker
+yum autoremove --assumeyes
 
 # Remove repository and files.
 rm -rf /etc/yum.repos.d/docker.repo /var/lib/docker
