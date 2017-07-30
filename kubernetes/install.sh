5218def371a4d307a85d03eb4acb400010674e5e
Marco Ebert <marco_ebert@icloud.com>
2017-07-30 22:50:48 +0200

    Kubernetes: Add repo_gpgcheck.

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -9,6 +9,7 @@ name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+repo_gpgcheck=1
 EOF
 
 # Install packages.
