8f98a624cd31bec4e264e8ef7779d6d2b5051a29
Marco Ebert <marco_ebert@icloud.com>
2017-08-03 13:47:16 +0200

    Setup: Make flannel configurable like master.yml.

--- setup/init.sh
+++ setup/init.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Check for existence of master.yml.
-if [ ! -f `dirname $0`/master.yml ]; then
+if [ ! -f `dirname $0`/master.yml ]
+then
   echo "master.yml not found. Copy and configure from master-template.yml."
+  exit 1
+fi
+
+# Check for existence of flannel/flannel-configmap.yml.
+if [ ! -f `dirname $0`/flannel/flannel-configmap.yml ]
+then
+  echo "flannel/flannel-configmap.yml not found. Copy and configure from flannel/flannel-configmap-template.yml."
   exit 1
 fi
 
@@ -22,4 +30,5 @@ chmod 600 $HOME/.kube/config
 
 # Apply flannel.
 kubectl apply -f `dirname $0`/flannel/flannel-rbac.yml
+kubectl apply -f `dirname $0`/flannel/flannel-configmap.yml
 kubectl apply -f `dirname $0`/flannel/flannel.yml
