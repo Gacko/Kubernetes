ed46c394a6ee91d6b32ecc5678b5a5156995fc7e
Marco Ebert <marco_ebert@icloud.com>
2017-09-18 10:22:51 +0200

    kube.sh: Add comments.

--- kube.sh
+++ kube.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-FILE="$1"
+# Define stage and task.
+STAGE="$1"
 TASK="$2"
-
+# Shift arguments.
 shift ; shift
 
+# Execute task on hosts.
 for host in $@
 do
-  ssh "$host" "rm -rf $FILE"
-  scp -r "$FILE" "$host:"
-  ssh "$host" "$FILE/$TASK.sh"
+  # Remove existing stage files.
+  ssh "$host" "rm -rf $STAGE"
+  # Transfer new stage files.
+  scp -r "$STAGE" "$host:"
+  # Execute task.
+  ssh "$host" "$STAGE/$TASK.sh"
 done
