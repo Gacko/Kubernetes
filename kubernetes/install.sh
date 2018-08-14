02c2c180c5eb42b77a48fee4b6883111146555c6
Marco Ebert <marco_ebert@icloud.com>
2018-08-14 10:43:06 +0200

    Kubernetes: Update to v1.9.10.

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define version.
-VERSION="1.8.13"
+VERSION="1.9.10"
 
 # Add repository.
 cp "$(dirname "$0")/kubernetes.repo" /etc/yum.repos.d/kubernetes.repo
@@ -12,10 +12,10 @@ cp "$(dirname "$0")/bridge-nf-call-iptables.conf" /etc/sysctl.d/90-bridge-nf-cal
 sysctl --system
 
 # Install packages.
-yum install --assumeyes "kubelet-$VERSION" "kubeadm-$VERSION" "kubectl-$VERSION" kubernetes-cni-0.5.1
+yum install --assumeyes "kubelet-$VERSION" "kubeadm-$VERSION" "kubectl-$VERSION" kubernetes-cni-0.6.0
 
 # Add version locks.
-yum versionlock add "kubelet-$VERSION" "kubeadm-$VERSION" "kubectl-$VERSION" kubernetes-cni-0.5.1
+yum versionlock add "kubelet-$VERSION" "kubeadm-$VERSION" "kubectl-$VERSION" kubernetes-cni-0.6.0
 
 # Install bash completion.
 kubectl completion bash > /etc/bash_completion.d/kubectl
