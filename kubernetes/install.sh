7818462007c23b99cd0fdb72e6f4e9814c9d871e
Marco Ebert <marco_ebert@icloud.com>
2021-02-23 15:45:55 +0100

    Update Kubernetes to 1.20.4.

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define version.
-VERSION="1.20.2"
+VERSION="1.20.4"
 
 # Enable bridge-nf-call-iptables.
 cp "$(dirname "$0")/bridge-nf-call-iptables.conf" /etc/sysctl.d/90-bridge-nf-call-iptables.conf
